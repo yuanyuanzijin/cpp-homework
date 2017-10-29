c81ee923b54c1922dca5afb7967b2ffe61dfc39a
Louie <jinluyuan@vip.qq.com>
2017-10-29 16:56:10 +0800

    add package

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+    int v;
+    public:
+    A* next;
+    void Func() { }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
